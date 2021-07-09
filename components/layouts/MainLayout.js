97b90ee5fd153a5e5323105390b9d724cd37a14c
jdshez <jd_shez@protonmail.com>
2021-07-09 23:57:52 +0100

    setup static files

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,7 +1,7 @@
-export default function(props) {
+export default function MainLayout(props) {
     return( <>
     <header>Our header</header>
     <main>{props.children}</main>
     <footer>Our footer</footer>
-    <>)
+    </>)
 }
